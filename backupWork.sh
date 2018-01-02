bb4a3a8381445b11187e2c2add7d88758e9be8ad
Kevin / Trancor <trancor@metal-asylum.net>
2018-01-01 19:59:03 -0500

    Create backupWork.sh
    This is more of a stable version saver.  You should look into SVNs and CVSs for proper file versioning. Or Snapshot for time based backups.

---
+++ backupWork.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# This is more of a stable version saver.
+# You should look into SVNs and CVSs for proper file versioning.
+# Or Snapshot for time based backups on Linux. 
+
+### backupWork Info ###
+# Reads *localPath*/work.txt for list of files to backup
+# Requires folder - backups
+# Copies file to - backups/*fileName*.ext.bk#
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+	bkFind=`ls backups/ | grep -c $line.bk`
+	from=$PWD/$line
+	echo $from
+	echo "to"
+	dest="$PWD/backups/$line.bk$((bkFind+1))"
+	echo $dest
+	cp $from $dest
+	echo "----------------"
+done < "$1"
