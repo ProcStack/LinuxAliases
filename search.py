0ea2e4db34b1856ea29af7702fffc383c467f027
Kevin / Trancor <trancor@metal-asylum.net>
2018-01-01 20:16:25 -0500

    Update search.py
    Quick internet search from terminal

--- search.py
+++ search.py
@@ -1,15 +1,16 @@
 # Add your desired search command to the aliases file; mine is here ~/.bash_aliases
-# alias search='python /media/thumb/core-python/search.py '
+# alias search='python /path/to/your/python/scripts/folder/search.py '
 # alias s='search '
 
 import sys, os
 argv="+".join(sys.argv[1:])
 msg=" ".join(sys.argv[1:])
 print "Searching for - " + msg
-#Change this to your search of choice
 
+# Set your browser command-
 browser='firefox'
 
+# Change this to your search of choice
 os.system(browser+' https://www.google.com/search?q='+argv)
 #os.system(browser+' https://duckduckgo.com/?q='+argv)
 #os.system(browser+' http://www.bing.com/search?q='+argv)
